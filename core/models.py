da44004666f190b4d86077f4fece9f8aee276a1d
LeonR <leonjy92@gmail.com>
2025-03-07 22:42:04 +0100

    cp

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, DateTime, func
+
+class TimestampMixin:
+    """Mixin to add timestamp fields to models."""
+    
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=True, onupdate=func.now())
+    changed_at = Column(DateTime, nullable=True)
+    deleted_at = Column(DateTime, nullable=True)
