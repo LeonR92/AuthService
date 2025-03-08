7ced57ec42bf8ed3f564b6e33e32aebf8100fe5c
LeonR <leonjy92@gmail.com>
2025-03-08 14:02:51 +0100

    ruff formatting

--- blueprints/users/models.py
+++ blueprints/users/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from core.database import Base
 from core.models import TimestampMixin
