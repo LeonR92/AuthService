2372241b7b1a3ce03945a1b98e97d803a28c8a8a
LeonR <leonjy92@gmail.com>
2025-03-08 09:42:36 +0100

    init dual db

--- blueprints/users/user_service.py
+++ blueprints/users/user_service.py
@@ -1,4 +1,4 @@
-from core.database import get_db
+from core.database import get_read_db,get_write_db
 from blueprints.users.user_repository import UserRepository
 
 class UserService:
@@ -6,18 +6,18 @@ class UserService:
 
     def register_user(self, email: str, name: str):
         """Registers a new user."""
-        with get_db() as db:  
+        with get_write_db() as db:  
             user_repo = UserRepository(db)
             return user_repo.create(email=email, name=name)
 
     def get_user(self, user_id: int):
         """Fetch a user by ID."""
-        with get_db() as db:
+        with get_read_db() as db:
             user_repo = UserRepository(db)
             return user_repo.get_by_id(user_id)
 
     def create_user(self):
-        with get_db() as db:
+        with get_write_db() as db:
             user_repo = UserRepository(db)
         # form validator
         # create credentials
