175577a220b362f7ef15e660e39ea833e1022ec0
LeonR <leonjy92@gmail.com>
2025-03-08 17:46:56 +0100

    formatted

--- tests/users/test_mfa_service.py
+++ tests/users/test_mfa_service.py
@@ -1,6 +1,5 @@
 import pytest
 from unittest.mock import MagicMock
-import pyotp
 from blueprints.users.mfa_service import MFAservice
 from blueprints.users.mfa_repository import MFARepository
 from blueprints.users.models import MFA
