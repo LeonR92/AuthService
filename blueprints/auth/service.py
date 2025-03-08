edd0abf830546a8aa61274ab5957104587f331be
LeonR <leonjy92@gmail.com>
2025-03-08 17:59:04 +0100

    init service

---
+++ blueprints/auth/service.py
@@ -0,0 +1,27 @@
+
+
+class AuthService():
+    
+    def check_password():
+        pass
+    
+    def verify_mfa():
+        pass
+
+    def reset_password():
+        pass
+
+    def change_email():
+        pass
+
+    def activate_mfa():
+        pass
+
+    def deactivate_mfa():
+        pass
+
+    def resend_mfa_seed():
+        pass
+
+    def create_mfa_seed():
+        pass
