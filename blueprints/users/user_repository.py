912308c648581b9d0a24682abebd51def371a349
LeonR <leonjy92@gmail.com>
2025-03-12 11:56:30 +0100

    added Optional

--- blueprints/users/user_repository.py
+++ blueprints/users/user_repository.py
@@ -20,7 +20,7 @@ class UserRepository:
                 .filter(Credentials.email == email)
                 .first())
     
-    def get_username_by_userid(self, user_id: int) -> tuple[str, str] | None:
+    def get_username_by_userid(self, user_id: int) -> Optional[tuple[str, str]]:
             """Fetches first and last name from the database."""
             return (self.db_session.query(User.first_name, User.last_name)
                     .filter(User.id == user_id)
